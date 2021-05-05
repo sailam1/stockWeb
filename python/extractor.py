3f5913272d64cb10aeed316a9bf52fe9aa70af91
Sailam Mungilwar <mungilwarsailam@gmail.com>
2021-05-05 19:13:51 +0530

    v22

--- python/extractor.py
+++ python/extractor.py
@@ -11,9 +11,9 @@ try:
     import pandas as pd
     from yahoo_fin.stock_info import get_data
     from sklearn.preprocessing import MinMaxScaler
-    from tensorflow.keras.models import Sequential
-    from tensorflow.keras.layers import Dense
-    from tensorflow.keras.layers import LSTM
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.layers import LSTM
     import numpy as np
 except Exception as e:
     print(e)
@@ -156,7 +156,6 @@ except Exception as e5:
 print(output_list)
 
 
-#0.960545
 
 
 
